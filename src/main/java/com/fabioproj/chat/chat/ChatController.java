66dcc8ea42609e3d0b9068c2e4aff47a1e58d957
Fabio Lima <fabiosdl@outlook.com>
2024-10-28 13:05:28 +0100

    backend working

---
+++ src/main/java/com/fabioproj/chat/chat/ChatController.java
@@ -0,0 +1,31 @@
+package com.fabioproj.chat.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.fabioproj.chat.chat.ChatMessage;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(
+            @Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+    SimpMessageHeaderAccessor headerAccessor) {
+
+        // add username in websocket section
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
---
+++ src/main/java/com/fabioproj/chat/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.fabioproj.chat.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}

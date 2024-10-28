fe76665b3063ca1f5aaa4a7940d2fa24fde456e9
Fabio Lima <fabiosdl@outlook.com>
2024-10-28 12:05:56 +0100

    initial commit

---
+++ src/main/java/com/fabioproj/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.fabioproj.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
